09ed6e85389ce76c852b870ba35e047530abccbf
romchat <k.romchat@gmail.com>
2018-09-27 20:07:11 +0700

    delete

--- s16page/statsite/s16stat/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import StatPage from './pages/facebookStat';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<StatPage />, document.getElementById('root'));
-registerServiceWorker();
